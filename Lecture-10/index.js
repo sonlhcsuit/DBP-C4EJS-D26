52c69d381ffe6373011275577069f574e8b23a60
sonlhcsuit <sonlh.cs.uit@gmail.com>
2021-05-08 20:56:24 +0700

    add example

---
+++ Lecture-10/index.js
@@ -0,0 +1,13 @@
+let button = document.querySelector("input[type=button]")
+let name = document.querySelector("input[type=text]")
+let select = document.querySelector("select")
+let output = document.querySelector("#output")
+// button.addEventListener("click",()=>{
+//     if (name.value != ""){
+//         output.innerHTML = `Your name is ${name.value
+//         }, your favorite pet is ${select.value}`
+//     }
+// })
+window.addEventListener("scroll",()=>{
+
+})
